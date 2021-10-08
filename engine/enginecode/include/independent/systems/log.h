e060c6e058f168ab6e38d37a7e9512f49868433d
Volity2001 <edwardlammasgames@gmail.com>
2021-10-08 14:56:16 +0100

    log

--- engine/enginecode/include/independent/systems/log.h
+++ engine/enginecode/include/independent/systems/log.h
@@ -9,46 +9,46 @@ namespace	Engine {
 		virtual void stop(SystemSignal close = SystemSignal::None, ...);
 
 		template <class ...Args>
-		static void info(Args&&...args);
+		static void info(Args&& ...args);
 
 		template <class ...Args>
-		static void error(Args&&...args);
+		static void error(Args&& ...args);
 
 		template <class ...Args>
-		static void warn(Args&&...args);
+		static void warn(Args&& ...args);
 
 		template <class ...Args>
-		static void trace(Args&&...args);
+		static void trace(Args&& ...args);
 
 		template <class ...Args>
-		static void debug(Args&&...args);
+		static void debug(Args&& ...args);
 	private:
 		static std::shared_ptr<spdlog::logger> consolelogger;
 	};
 
 	template<class ...Args>
-	static void Log::debug(Args&&...args) {
-		consolelogger->debug(std::forward<Args>(args))
+	static void Log::debug(Args&& ...args) {
+		consolelogger->debug(std::forward<Args>(args) ...);
 	}
 
 	template<class ...Args>
-	static void Log::error(Args&&...args) {
-		consolelogger->debug(std::forward<Args>(args))
+	static void Log::error(Args&& ...args) {
+		consolelogger->debug(std::forward<Args>(args) ...);
 	}
 
 	template<class ...Args>
-	static void Log::info(Args&&...args) {
-		consolelogger->debug(std::forward<Args>(args))
+	static void Log::info(Args&& ...args) {
+		consolelogger->debug(std::forward<Args>(args) ...);
 	}
 
 	template<class ...Args>
-	static void Log::trace(Args&&...args) {
-		consolelogger->debug(std::forward<Args>(args))
+	static void Log::trace(Args&& ...args) {
+		consolelogger->debug(std::forward<Args>(args) ...);
 	}
 
 	template<class ...Args>
-	static void Log::warn(Args&&...args) {
-		consolelogger->debug(std::forward<Args>(args))
+	static void Log::warn(Args&& ...args) {
+		consolelogger->debug(std::forward<Args>(args) ...);
 	}
 
 }
--- engine/enginecode/src/independent/log.cpp
+++ engine/enginecode/src/independent/log.cpp
@@ -3,14 +3,17 @@
 
 namespace Engine {
 	std::shared_ptr <spdlog::logger> Log::consolelogger = nullptr;
+
 	void Log::start(SystemSignal init, ...) {
 		spdlog::set_pattern("%^[%T]:%v%$");
 		spdlog::set_level(spdlog::level::trace);
-		consolelogger = spdlog::stderr_color_mt("console");
+		consolelogger = spdlog::stderr_color_mt("Console");
 	}
 
-	void Log::stop(SystemSignal init, ...) {
+	void Log::stop(SystemSignal close, ...) {
 		consolelogger->info("stopping");
 		consolelogger.reset();
 	}
+
+
 }
