4f98beb214e27e9291af650ca74d867c2ac169d7
Volity2001 <edwardlammasgames@gmail.com>
2021-10-08 15:15:10 +0100

    working file and console logger

--- engine/enginecode/include/independent/systems/log.h
+++ engine/enginecode/include/independent/systems/log.h
@@ -22,8 +22,12 @@ namespace	Engine {
 
 		template <class ...Args>
 		static void debug(Args&& ...args);
+
+		template <class ...Args>
+		static void file(Args&& ...args);
 	private:
 		static std::shared_ptr<spdlog::logger> consolelogger;
+		static std::shared_ptr<spdlog::logger> filelogger;
 	};
 
 	template<class ...Args>
@@ -33,22 +37,34 @@ namespace	Engine {
 
 	template<class ...Args>
 	static void Log::error(Args&& ...args) {
+#ifdef NG_DEBUG
 		consolelogger->debug(std::forward<Args>(args) ...);
+#endif // NG_DEBUG
 	}
 
 	template<class ...Args>
 	static void Log::info(Args&& ...args) {
+#ifdef NG_DEBUG
 		consolelogger->debug(std::forward<Args>(args) ...);
+#endif // NG_DEBUG
 	}
 
 	template<class ...Args>
 	static void Log::trace(Args&& ...args) {
+#ifdef NG_DEBUG
 		consolelogger->debug(std::forward<Args>(args) ...);
+#endif // NG_DEBUG
 	}
 
 	template<class ...Args>
 	static void Log::warn(Args&& ...args) {
+#ifdef NG_DEBUG
 		consolelogger->debug(std::forward<Args>(args) ...);
+#endif // NG_DEBUG
 	}
 
+	template<class ...Args>
+	static void Log::file(Args&& ...args) {
+		filelogger->trace(std::forward<Args>(args) ...);
+	}
 }
--- engine/enginecode/src/independent/log.cpp
+++ engine/enginecode/src/independent/log.cpp
@@ -3,11 +3,21 @@
 
 namespace Engine {
 	std::shared_ptr <spdlog::logger> Log::consolelogger = nullptr;
+	std::shared_ptr <spdlog::logger> Log::filelogger = nullptr;
 
 	void Log::start(SystemSignal init, ...) {
 		spdlog::set_pattern("%^[%T]:%v%$");
 		spdlog::set_level(spdlog::level::trace);
 		consolelogger = spdlog::stderr_color_mt("Console");
+
+		char filepath[256] = "logs/";
+		char time[128];
+		std::time_t t = std::time(nullptr);
+		std::strftime(time, sizeof(time), "%d_%m_%y %I_%M_%S", std::localtime(&t));
+		strcat_s(filepath, time);
+		strcat_s(filepath,".txt");
+
+		filelogger = spdlog::basic_logger_mt("File", filepath);
 	}
 
 	void Log::stop(SystemSignal close, ...) {
