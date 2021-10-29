be31f177a57a88633d6747ac6ae3a24306bd13a6
Volity2001 <edwardlammasgames@gmail.com>
2021-10-29 14:18:36 +0100

    is_function fixed to function

--- engine/enginecode/include/independent/core/events/EventHandler.h
+++ engine/enginecode/include/independent/core/events/EventHandler.h
@@ -5,7 +5,7 @@
 namespace Engine {
 	class EventHandler {
 	public:
-		void setonclosecallback(const std::is_function<bool(WindowCloseEvent&)>& fn) { m_onclosecallback = fn; }
+		void setonclosecallback(const std::function<bool(WindowCloseEvent&)>& fn) { m_onclosecallback = fn; }
 		std::function<bool(WindowCloseEvent&)> getonclosecallback() { return m_onclosecallback; }
 
 		
